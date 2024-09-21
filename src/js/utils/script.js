0b7ecb517f5381b04a7e7f061184458e0395b15f
vyazemskaya <vyazemskayanna@gmail.com>
2024-09-21 22:20:52 +0300

    dummy page

--- src/js/utils/script.js
+++ src/js/utils/script.js
@@ -1,1 +1,12 @@
+const handleDummyHeight = () => {
+  const dummy = document.querySelector(".dummy");
 
+  if (dummy.offsetHeight > window.innerHeight) {
+    document.documentElement.style.removeProperty("overflow");
+  } else {
+    document.documentElement.style.overflow = "hidden";
+  }
+};
+handleDummyHeight();
+
+window.addEventListener("resize", handleDummyHeight);
